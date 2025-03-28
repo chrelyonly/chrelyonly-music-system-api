400a6e87853842b900889df858c5f2466967a2e4
chrelyonly <1172576293@qq.com>
2025-03-28 15:03:24 +0800

    1

---
+++ src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/ChrelyonlyMusicSystemApiApplication.java
@@ -0,0 +1,16 @@
+package cn.chrelyonly.chrelyonlymusicsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 11725
+ */
+@SpringBootApplication
+public class ChrelyonlyMusicSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChrelyonlyMusicSystemApiApplication.class, args);
+    }
+
+}
