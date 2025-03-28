3100ec517866ddd0262450df9ff851492cfc6519
chrelyonly <1172576293@qq.com>
2025-03-28 16:55:58 +0800

    1

--- src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/config/MyConfig.java
+++ src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/config/MyConfig.java
@@ -10,10 +10,10 @@ public class MyConfig {
     public static String PHONE = "15008898674";
     public static String UUID = "Qe1dBF8AQH1hoE7Zo1EY";
     public static String TOKEN = "b41dcfe68a1d4a6390335bd5978739a3";
-//    开发用这个
-    public static String DOMAIN = "chrelyonly-music-system.frp.chrelyonly.cn";
 //    后端域名
-//    public static String DOMAIN = "172.18.0.9";
+    public static String DOMAIN = "172.18.0.9";
 //    音乐后端地址
-    public static String SERVER_URL_API = "http://" + DOMAIN + ":3000";
+//    public static String SERVER_URL_API = "http://" + DOMAIN + ":3000";
+//    测试地址
+    public static String SERVER_URL_API = "https://chrelyonly-music-system.frp.chrelyonly.cn";
 }
