bb7458b258b69d5aaf32fc4dc999010a5665c318
chrelyonly <1172576293@qq.com>
2025-04-03 13:20:00 +0800

    1

--- src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/qq/config/MyQqConfig.java
+++ src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/qq/config/MyQqConfig.java
@@ -10,8 +10,8 @@ public class MyQqConfig {
 //    后端域名
     public static String DOMAIN = "172.18.0.12";
 //    音乐后端地址
-//    public static String SERVER_URL_API = "http://" + DOMAIN + ":3000";
-    public static String SERVER_URL_API = "http://127.0.0.1:3200";
+    public static String SERVER_URL_API = "http://" + DOMAIN + ":3200";
+//    public static String SERVER_URL_API = "http://127.0.0.1:3200";
 //    测试地址
 //    public static String SERVER_URL_API = "https://chrelyonly-music-system.frp.chrelyonly.cn";
 }
