9f7ebea31b6ad30314fc70e2f3d6735d7eb82ac9
chrelyonly <1172576293@qq.com>
2025-06-11 11:11:11 +0800

    1

--- src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/music/kg/config/MyKgConfig.java
+++ src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/music/kg/config/MyKgConfig.java
@@ -1,5 +1,6 @@
 package cn.chrelyonly.chrelyonlymusicsystemapi.music.kg.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,12 +8,14 @@ import org.springframework.context.annotation.Configuration;
  * @author 11725
  */
 @Configuration
+@Slf4j
 public class MyKgConfig {
     //    服务器地址
     public static String SERVICE_URL;
 
     @Value("${music.kg.serviceUrl}")
     public void setServiceUrl(String serviceUrl) {
+        log.info("初始化KG音乐插件");
         MyKgConfig.SERVICE_URL = serviceUrl;
     }
 
--- src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/music/wyy/config/MyWyyConfig.java
+++ src/main/java/cn/chrelyonly/chrelyonlymusicsystemapi/music/wyy/config/MyWyyConfig.java
@@ -1,5 +1,6 @@
 package cn.chrelyonly.chrelyonlymusicsystemapi.music.wyy.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * @author 11725
  */
 @Configuration
+@Slf4j
 public class MyWyyConfig {
 //    服务器地址
     public static String SERVICE_URL;
@@ -15,6 +17,7 @@ public class MyWyyConfig {
 
     @Value("${music.wyy.serviceUrl}")
     public void setServiceUrl(String serviceUrl) {
+        log.info("初始化WYY音乐插件");
         MyWyyConfig.SERVICE_URL = serviceUrl;
     }
 }
